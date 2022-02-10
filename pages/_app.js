32ac758f9cf068e2ac1920ad4db322092334d834
Cindy Giron <35552219+mizzrawr@users.noreply.github.com>
2022-02-10 19:00:23 +0100

    Add basic layout, nextjs w/apollo & Chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
